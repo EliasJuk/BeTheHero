dc076ddc7a7188ca95d24ed8da19b44fa6d8e652
eliasjuk <eliasp.juk@hotmail.com>
2020-03-24 17:38:21 -0300

    Criado rota de login

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,7 +2,7 @@ const express = require("express")
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
 const ProfileController = require('./controllers/ProfileController')
-
+const SessionController = require('./controllers/SessionController')
 
 const routes = express.Router()
 
@@ -11,6 +11,10 @@ const routes = express.Router()
     routes.get("/", (request, response) => {           
         response.send("Hello, World!")
     })
+
+    //SESSIONS
+    routes.post('/sessions', SessionController.create)
+
     //USERS    
     routes.get('/ongs', OngController.index)
     routes.post('/ongs', OngController.create)
