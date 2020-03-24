49071db7f13b5db8327335053ba9b4cc183b3e5d
eliasjuk <eliasp.juk@hotmail.com>
2020-03-24 10:58:55 -0300

    Testando rotas com insomnia

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # BeTheHero
 > Aplicação desenvolvida durante a **Semana OmniStack 11.0**
 
-
 ## BACKEND
 > nodemon index.js
 
 ## FRONTEND
 > npm start
+
+## TESTAR ROTAS
+> Para testar as rotas podemos utilizar o insomnia
+> https://insomnia.rest/
 
 ### Download NodeJs
 > https://nodejs.org/en/download/package-manager/#windows
--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,43 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+
+/*
+*   Método HTTP:
+*
+*   GET: buscar uma informação do back-end
+*   POST: Cria uma informação no back-end
+*   PUT: Alterar uma informação no backend
+*   DELETE: Deletar uma informação no backend
+*/
+
+/**
+ *  Tipo de parãmetros
+ *  
+ *  Query Params: Parãmetros nomeados na rota após "?" (filtros, paginação) => req.query
+ *  Route Params: Parãmetros utilizados para identificar recursos => req.params
+ *  Request Body: Corpo da requisição, utilizada para criar ou alterar recursos => req.body
+ */
+
 //ROUTES
-    app.get("/", (req, res) => {
-        return res.json({ 
-            evento: 'Semana OmniStack 11.0',
-            aluno: 'Elias Juk'
+    //MAIN
+        app.get("/", (req, res) => {           
+            return res.json({ 
+                evento: 'Semana OmniStack 11.0',
+                aluno: 'Elias Juk',
+            })
         })
-    })
+    //USERS
+        app.post("/users", (req, res) => {
+            const body = req.body
+            console.log(body)
+
+            return res.json({
+                evento: 'Semana OmniStack 11.0',
+                aluno: 'Elias Juk'
+            })
+        })
     
 //CONFIG
     //SERVER
