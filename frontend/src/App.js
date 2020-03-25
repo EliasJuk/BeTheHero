bd2e5cd1a5e39bb8faea575dbd63ae00a99d4220
eliasjuk <eliasp.juk@hotmail.com>
2020-03-25 10:20:19 -0300

    Criado o arquivo de estilo global

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,13 @@
-import React, {useState} from 'react';
-import Header from './Header'
+import React from 'react';
+import Logon from './pages/Logon'
+
+import './global.css'
 
 // JSX (JavaScript XML)
 function App() {
-  const [counter, setCounter] = useState(0)
-
-  function increment(){
-    setCounter(counter + 1)
-
-    console.log(counter)
-  }
-
+  
   return (
-    <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
+    <Logon />
   )
 }
 
---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './style.css'
+import heroesImg from '../../assets/heroes.png'
+
+export default function Logon() {
+    return (
+        <div className="logon-container">
+            <section className="form">
+                
+            </section>
+            <img src={heroesImg} />
+        </div>
+    )
+}
