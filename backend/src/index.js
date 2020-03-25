e402e3e4380fbc1d1d49622d7f5edf936ee4eee9
eliasjuk <eliasp.juk@hotmail.com>
2020-03-25 09:01:31 -0300

    Importação do modulo CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,16 @@
 const express = require("express")
+const cors = require('cors')
 const routes = require('./routes')
+
 const app = express()
 
 //INITIALS CONFIG
+    //CORS
+        app.use(cors())
     //JSON  
         app.use(express.json())
     //ROUTES
         app.use(routes)
-
-
 
 //CONFIG
     //SERVER
