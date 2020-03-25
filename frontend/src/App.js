52f2427269dd133261748a5294e2d59f1a3fd671
eliasjuk <eliasp.juk@hotmail.com>
2020-03-25 11:06:03 -0300

    Definir rotas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Logon from './pages/Logon'
+
+import Routes from './routes'
 
 import './global.css'
 
@@ -7,7 +8,7 @@ import './global.css'
 function App() {
   
   return (
-    <Logon />
+    <Routes />
   )
 }
 
