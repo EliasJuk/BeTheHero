ecaa58f816905298b10f7ca9b3d51cd697e72e4c
eliasjuk <eliasp.juk@hotmail.com>
2020-03-26 15:14:10 -0300

    Remover todos arquivos da aplicação mobile

--- mobile/src/Detail/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { view, View } from 'react-native'
-
-export default function Detail() {
-    return (
-        <View />
-    )
-}
--- mobile/src/Incidents/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { view, View } from 'react-native'
-
-export default function Incidents() {
-    return (
-        <View />
-    )
-}
